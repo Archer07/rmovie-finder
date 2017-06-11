403cf9b8d64a8dc8a237a398bae577a23a43384f
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-11 11:49:31 +0000

    Setting up the Dispatcher

---
+++ src/js/dispatchers/AppDispatcher.js
@@ -0,0 +1,17 @@
+// Flux Dispatcher Setup
+
+const Dispatcher = require('flux').Dispatcher;
+const assign = require('object-assign');
+
+
+let AppDispatcher = assign(new Dispatcher(), {
+  handleViewAction: function(action) {
+    let payload = {
+      source: 'VIEW_ACTION',
+      action: action
+    }
+    this.dispatch(payload);
+  }
+});
+
+module.exports = AppDispatcher;
